51d61d970756aff27da23941a83abd563c77480d
mlukacs <mlukacs@deezer.com>
2021-09-13 20:02:16 +0200

    added version of package and crud actions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Jolt",
+    platforms: [
+        .iOS(.v14),
+        .watchOS(.v7)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
