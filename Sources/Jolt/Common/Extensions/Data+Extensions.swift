1107e5dde0c6edeb7ffa0d5ad91eacf472a190d5
mlukacs <mlukacs@deezer.com>
2021-10-08 18:24:04 +0200

    clean of code

--- Sources/Jolt/Common/Extensions/Data+Extensions.swift
+++ Sources/Jolt/Common/Extensions/Data+Extensions.swift
@@ -11,4 +11,23 @@ extension Data {
     func decodeToString(with encodingType: String.Encoding = .utf8) -> String {
         String(data: self, encoding: encodingType) ?? ""
     }
+    
+    static func multipartFormData(with paramConfig: RequestConfigs, and boundary: String) -> Data {
+        var bodyData = Data()
+        
+        if let parameters = paramConfig.parameters as? [String: Any] {
+            bodyData = parameters.buildBodyPart(boundary: boundary)
+        }
+
+        if let parts = paramConfig.parts {
+            for var part in parts {
+                part.boundary = boundary
+                bodyData.append(part.formData)
+            }
+        }
+        
+        bodyData.append("--\(boundary)--\r\n".data(using: .utf8)!)
+        
+        return bodyData
+    }
 }
--- Sources/Jolt/Common/Extensions/Publisher+Extensions.swift
+++ Sources/Jolt/Common/Extensions/Publisher+Extensions.swift
@@ -19,3 +19,59 @@ extension Publisher where Output == Data {
         decode(type: type, decoder: decoder)
     }
 }
+
+extension Publisher where Output == URLSession.DataTaskPublisher.Output {
+    func validateDataResponse() -> Publishers.TryMap<Self, Data> {
+        tryMap { element -> Data in
+            guard let httpResponse = element.response as? HTTPURLResponse,
+                  200...299 ~= httpResponse.statusCode
+            else {
+                throw JoltNetworkErrors.network("\((element.response as? HTTPURLResponse)?.statusCode ?? -42)")
+            }
+            return element.data
+        }
+    }
+    
+    func validateGenericResponse<ReturnType>() -> Publishers.TryMap<Self, ReturnType> {
+        tryMap { element -> ReturnType in
+            guard let httpResponse = element.response as? HTTPURLResponse,
+                  200...299 ~= httpResponse.statusCode
+            else {
+                throw JoltNetworkErrors.network("\((element.response as? HTTPURLResponse)?.statusCode ?? -42)")
+            }
+            if ReturnType.self is Void.Type {
+                return Void() as! ReturnType
+            } else if ReturnType.self is Data.Type {
+                return element.data as! ReturnType
+            }
+            guard let returnObject = try JSONSerialization.jsonObject(with: element.data, options: []) as? ReturnType else {
+                throw JoltNetworkErrors.mismatchErrorInReturnType
+            }
+            
+            return returnObject
+        }
+    }
+    
+    func logURLResponse(with logger: JoltLogging) -> Publishers.Map<Self, URLSession.DataTaskPublisher.Output> {
+        map { element -> URLSession.DataTaskPublisher.Output in
+            logger.log(response: element.response, and: element.data)
+            return element
+        }
+    }
+}
+
+extension Publisher {
+    func logError(with logger: JoltLogging, request: URLRequest, and requestConfig: RequestConfigs)
+    -> Publishers.HandleEvents<Self> {
+        handleEvents(receiveCompletion: { completion in
+            switch completion {
+            case .failure(let error):
+                logger.logError(requestConfig: requestConfig,
+                                     request: request,
+                                     error: error)
+            case .finished:
+                break
+            }
+        })
+    }
+}
